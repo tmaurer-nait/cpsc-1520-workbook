a62c0f850c6b08df89dab85db4896a91c5b0d2bc
Tom Maurer <tmaurer@nait.ca>
2024-10-02 19:12:00 -0600

    Week 5 day 1

--- Week4-day2/forms-validation-START/js/main.js
+++ Week4-day2/forms-validation-START/js/main.js
@@ -25,53 +25,167 @@ let runningTotal = 0;
 // The reason we are not using document.querySelector() is because the form elements (or inputs) won't always have ids
 // or other selectable attributes. but they should always have names
 budgetFormElement.elements["budget-title"].focus();
+
 // document.querySelector("#budget-item").focus();
 
 // add a submit listener
 budgetFormElement.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  // Get the form input elements
+  let titleElement = event.target.elements["budget-title"];
+  let amountElement = event.target.elements["amount"];
+  let descriptionElement = event.target.elements["budget-description"];
+
   // Get the form input values
-  let titleValue = budgetFormElement.elements["budget-title"].value;
-  let amountValue = budgetFormElement.elements["amount"].value;
-  let descriptionValue = budgetFormElement.elements["budget-description"].value;
+  let titleValue = titleElement.value;
+  let amountValue = amountElement.value;
+  let descriptionValue = descriptionElement.value;
 
-  // TODO: Validate the form inputs
+  // Validate the form inputs
 
-  // Add a line items
-  addLineItem(titleValue, amountValue, descriptionValue);
+  // Validate the title input
+  if (isNotEmpty(titleValue)) {
+    titleElement.classList.remove("is-invalid");
+  } else {
+    titleElement.classList.add("is-invalid");
+  }
 
-  // Reset the form
-  // Method 1
-  // budgetFormElement.elements["budget-title"].value = "";
-  // budgetFormElement.elements["amount"].value = "";
-  // budgetFormElement.elements["budget-description"].value = "";
+  // Validate the description
+  if (isNotEmpty(descriptionValue)) {
+    descriptionElement.classList.remove("is-invalid");
+  } else {
+    descriptionElement.classList.add("is-invalid");
+  }
 
-  // Method 2
-  budgetFormElement.reset();
+  // Validate the amount
+  if (isGreaterThanZero(parseFloat(amountValue))) {
+    amountElement.classList.remove("is-invalid");
+  } else {
+    amountElement.classList.add("is-invalid");
+  }
 
-  // Refocus the initial element
-  budgetFormElement.elements["budget-title"].focus();
+  if (isValidForm(titleValue, descriptionValue, parseFloat(amountValue))) {
+    // Add a line items
+    addLineItem(titleValue, parseFloat(amountValue), descriptionValue);
 
-  // Update the budget total
-  addToTotal(parseFloat(amountValue));
+    // Reset the form
+    // Method 1
+    // budgetFormElement.elements["budget-title"].value = "";
+    // budgetFormElement.elements["amount"].value = "";
+    // budgetFormElement.elements["budget-description"].value = "";
+
+    // Method 2
+    budgetFormElement.reset();
+
+    // Refocus the initial element
+    budgetFormElement.elements["budget-title"].focus();
+
+    // Update the budget total
+    addToTotal(parseFloat(amountValue));
+  }
 });
 
 // function to add a budget item create function
 // e.g.
 // <li class="list-group-item list-group-item-action" aria-current="true">
 // TITLE AMOUNT - DESCRIPTION</li>
-
 const addLineItem = (title, amount, description) => {
-  let newLineItem = `<li class="list-group-item list-group-item-action" aria-current="true">${title} ${amount} - ${description}</li>`;
-  budgetListElement.innerHTML += newLineItem;
+  let newLineItem = `<li class="list-group-item list-group-item-action" aria-current="true">${title} ${moneyFormat(
+    amount
+  )} - ${description}</li>`;
+  budgetListElement.innerHTML = budgetListElement.innerHTML + newLineItem;
 };
 
 // function to add a updateTotal function
 function addToTotal(amount) {
   runningTotal += amount;
-  // TODO: Format this to look nicer
-  budgetTotalElement.innerText = runningTotal;
+  // Before updating the inner text format it to look nice (add the dollar sign)
+  budgetTotalElement.innerText = moneyFormat(runningTotal);
 }
 
 // function for general currency formatter
+function moneyFormat(amount) {
+  // This would be the more professional way to do this but is also more complicated
+  // let formatter = Intl.NumberFormat("en-CA", {
+  //   style: "currency",
+  //   currency: "CAD",
+  // });
+
+  // let formattedAmount = formatter.format(amount);
+
+  let formattedAmount = `$${amount}`;
+  return formattedAmount;
+}
+
+// Validator function that returns true if text is not empty else false
+const isNotEmpty = (text) => {
+  // Start with validity being false (will change to true if valid)
+  let isValid = false;
+
+  // One way to check if text is empty
+  // if (text !== "") {
+  //   isValid = true;
+  // }
+
+  // Checking if text is longer than 0 characters
+  if (text.length > 0) {
+    isValid = true;
+  }
+
+  return isValid;
+};
+
+// Validator function that returns true if value is greater than 0 else false
+function isGreaterThanZero(value) {
+  if (value > 0) {
+    return true;
+  } else {
+    return false;
+  }
+
+  // Other cleaner ways to do this
+  // return value > 0 ? true : false;
+  // return value > 0;
+}
+
+// Validate all 3 fields on the form
+const isValidForm = (title, description, amount) => {
+  let isValid = false;
+
+  // If amount is less than or equal to zero set isValid to false
+  if (isGreaterThanZero(amount) === false) {
+    isValid = false;
+  }
+
+  // If title is empty set isValid to false
+  // Using the ! to flip the boolean instead of checking for false (works either way)
+  if (!isNotEmpty(title)) {
+    isValid = false;
+  }
+
+  // If description is empty set isValid to false
+  if (isNotEmpty(description) === false) {
+    isValid = false;
+  }
+
+  // Using an OR
+  // if (
+  //   isGreaterThanZero(amount) == false ||
+  //   isNotEmpty(title) == false ||
+  //   isNotEmpty(description) == false
+  // ) {
+  //   isValid = false;
+  // }
+
+  //Using an AND
+  // if (
+  //   isGreaterThanZero(amount) &&
+  //   isNotEmpty(title) &&
+  //   isNotEmpty(description)
+  // ) {
+  //   isValid = true;
+  // }
+
+  return isValid;
+};
